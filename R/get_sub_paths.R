2fe12edb66fb88934fe5e8e842e3985f525d6c78
Soeren Petersen <soerendpetersen@gmail.com>
2022-07-08 08:49:21 +0200

    fix := rlang namesspace error

--- R/get_sub_paths.R
+++ R/get_sub_paths.R
@@ -4,18 +4,21 @@
 #' @param pattern A regex.
 #' @param col_name A string.
 #' @return tibble::tibble with character column col_name containing abs_paths of subfiles
+#' @importFrom rlang :=
 #' @export
 #' @examples
 #' get_sub_paths("./data", pattern = ".csv", col_name = "abs_path")
-#' library(readutils); readutils::get_sub_paths(path = "/Users/sorpet/Desktop/data",
-#' pattern = ".csv",
-#' col_name = "abs_path")
+#' library(readutils)
+#' readutils::get_sub_paths(
+#'   path = "/Users/sorpet/Desktop/data",
+#'   pattern = ".csv",
+#'   col_name = "abs_path"
+#' )
 get_sub_paths <- function(path, pattern, col_name = "abs_path") {
   # obs create functionality if path point to single file instead of folder
   path |>
     base::list.files(pattern = pattern, full.names = TRUE) |>
-    #tibble::tibble(!!col_name := .) %>% #doesn't work due to |> instead of %>%
+    # tibble::tibble(!!col_name := .) %>% #doesn't work due to |> instead of %>%
     (function(.) tibble::tibble(!!col_name := .))() |>
     identity()
 }
-
